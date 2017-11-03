d8c2e86cc90525303235d2c488ab5192c2aa125e
annclair <anneclaire.sebirou@gmail.com>
2017-11-03 16:53:18 +0100

    install material ui, icons, create navbar and barliist components

---
+++ src/components/BarList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class BarList extends Component {
+    render() {
+        return (
+            <div>BarList</div>
+        );
+    }
+}
+
+export default BarList;
---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
+const styles = theme => ({
+    root: {
+        width: '100%',
+    },
+    flex: {
+        flex: 1,
+    }
+});
+
+function SimpleAppBar(props) {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" >
+                <Toolbar>
+                    <Typography type="title" color="inherit" className={classes.flex}>
+                        Les bars de Bordeaux
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+SimpleAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleAppBar);
