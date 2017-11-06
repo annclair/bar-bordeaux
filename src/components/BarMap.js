a3be5394db01c340f5cc15ce3c8d7bb025cbd24b
annclair <anneclaire.sebirou@gmail.com>
2017-11-06 13:11:34 +0300

    add leaflet map

--- src/components/BarMap.js
+++ src/components/BarMap.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
 class BarMap extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            lat: 44.837912,
+            lng: -0.579541,
+            zoom: 13,
+        };
+    }
+
     render() {
+        const position = [this.state.lat, this.state.lng];
         return(
-            <div><h1>Carte des bars</h1></div>
+            <div>
+                <h1>Carte des bars</h1>
+                <Map center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Marker position={position}>
+                        {/* <Popup>
+                            <span>A pretty CSS3 popup.
+                                <br />
+                                Easily customizable.
+                            </span>
+                        </Popup> */}
+                    </Marker>
+                </Map>
+            </div>
         )
     }
 }
-
 
 export default BarMap;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,7 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
 
 const styles = theme => ({
     root: {
@@ -25,6 +26,8 @@ function SimpleAppBar(props) {
                         <Typography type="title" color="inherit" className={classes.flex}>
                             <Link to='/'>Les bars de Bordeaux</Link>
                         </Typography>
+                        <Button color="contrast"><Link to='/'>Liste</Link></Button>
+                        <Button color="contrast"><Link to='/map'>Carte</Link></Button>
                 </Toolbar>
             </AppBar>
         </div>
